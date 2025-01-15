bb24b109f941366ae0fd40e155cec0c0a5359418
Joao Oliveira <joao.oliveira@directy.com.br>
2025-01-15 14:45:14 -0300

    app.ts configurado

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -10,8 +10,25 @@ router.get("/produtos", async (__, res) =>{
 });
 
 //Adicionar um produto 
-router.post("/produtos", async (_,res) => {
+router.post("/produtos", async (req, res) => {
     const { nome, categoria, preco, estoque } = req.body;
     await db.query("INSERT INTO produtos (nome, categoria, preco, estoque) VALUES (?, ?, ?, ?)", [nome, categoria, preco, estoque]);
     res.status(201).send("Produto adicionado!"); 
 })
+
+//Atualizar um produto 
+router.put("/produtos/:id", async (req, res) => {
+    const { id } = req.params;
+    const { nome, categoria, preco, estoque } = req.body;
+    await db.query("UPDATE produtos SET nome = ?, categoria = ?, preco = ?, estoque = ? WHERE id = ?", [nome, categoria, preco, estoque, id]);
+    res.send("Produto atualizado!");
+});
+
+//Deletar um produto
+router.delete("/produtos/:id", async (req, res) => {
+    const { id } = req.params;
+    await db.query("DELETE FROM produtor where id = ?", [id]);
+    res.send("Produto deletado!");
+});
+
+export default router;
